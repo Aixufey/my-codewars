e79975c6bdef8c40a65f69da140de493212f078f
Jack Xia <63552230+Aixufey@users.noreply.github.com>
2023-06-04 07:52:13 +0000

    flowers

---
+++ 8kyu/flowers.js
@@ -0,0 +1,10 @@
+
+
+// Two person are in love if the sum of the flower petals are odd
+// Example a flower has petals of 3, 5 or 6
+// if the sum of two flowers petals added together is a odd number, they are in love.
+
+
+function inLove(flower1, flower2) {
+    return flower1 % 2 + flower2 % 2 == 1
+}
